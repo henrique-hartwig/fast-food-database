c9b5691daa92da6f8bf54a391e66174b5eae28d9
Henrique Hartwig <henrique2805@gmail.com>
2025-03-22 17:22:15 -0300

    fix: remove comments and default_tag

--- data.tf
+++ data.tf
@@ -1,17 +1,10 @@
-# Verifica se a VPC existe
 data "aws_vpc" "existing" {
   filter {
     name   = "tag:Name"
     values = ["${var.project_name}-vpc"]
   }
-  
-  # Evita erro se não encontrar a VPC
-  default_tags {
-    tags = {}
-  }
 }
 
-# Verifica se as subnets existem
 data "aws_subnets" "private" {
   filter {
     name   = "vpc-id"
@@ -26,7 +19,6 @@ data "aws_subnets" "private" {
   depends_on = [data.aws_vpc.existing]
 }
 
-# Verifica se o security group existe
 data "aws_security_group" "rds" {
   filter {
     name   = "vpc-id"
@@ -41,12 +33,10 @@ data "aws_security_group" "rds" {
   depends_on = [data.aws_vpc.existing]
 }
 
-# Verifica se o DB subnet group existe
 data "aws_db_subnet_group" "existing" {
   name = "${var.project_name}-rds-subnet-group"
 }
 
-# Verifica se a instância RDS existe
 data "aws_db_instance" "existing" {
   db_instance_identifier = "${var.project_name}-db"
 } 
